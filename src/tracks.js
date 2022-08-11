60f0d5fa03e1c94474da4fabc56538784323535e
Ethan D-P <ethan@reth.com>
2022-08-11 12:10:09 +0100

    Working MVP

---
+++ src/tracks.js
@@ -0,0 +1,13 @@
+import imgSrc from "./assets/artwork.jpg";
+import AJR from "./assets/AJRIWON'T.mp3";
+
+
+export default [
+  {
+    title: "I Won't",
+    artist: "AJR",
+    audioSrc: AJR,
+    image: imgSrc,
+    color: "#00aeb0"
+  },
+];
