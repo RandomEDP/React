6b2aed4ee8d42166e0ed7890b8dc4b97597ffc70
Ethan D-P <ethan@reth.com>
2022-08-11 14:32:52 +0100

    Added Shuffle Function

--- src/tracks.js
+++ src/tracks.js
@@ -1,13 +1,30 @@
 import imgSrc from "./assets/artwork.jpg";
-import AJR from "./assets/AJRIWON'T.mp3";
-
+import imgSrc2 from "./assets/artwork2.jpg";
+import imgSrc3 from "./assets/artwork3.jpg";
+import AJRIWont from "./assets/AJRIWON'T.mp3";
+import WorldsmallestViolin from "./assets/AJR-World's-Smallest-Violin.mp3";
+import Bang from "./assets/BANG.mp3";
 
 export default [
   {
     title: "I Won't",
     artist: "AJR",
-    audioSrc: AJR,
+    audioSrc: AJRIWont,
     image: imgSrc,
+    color: "#00aeb0"
+  },
+  {
+    title: "World's smallest Violin",
+    artist: "AJR",
+    audioSrc: WorldsmallestViolin,
+    image: imgSrc2,
+    color: "#00aeb0"
+  },
+  {
+    title: "Bang!",
+    artist: "AJR",
+    audioSrc: Bang,
+    image: imgSrc3,
     color: "#00aeb0"
   },
 ];
